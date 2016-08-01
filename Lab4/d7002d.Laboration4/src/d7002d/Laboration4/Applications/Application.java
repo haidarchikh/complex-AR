9a9dbfc748c7390e5ce9961b414a15016cca1712
Haidar Chikh <haidarchikh@gmail.com>
2016-08-01 13:58:45 +0200

    split the workspace

--- Lab4/d7002d.Laboration4/src/d7002d/Laboration4/Applications/Application.java
+++
@@ -1,9 +0,0 @@
-package d7002d.Laboration4.Applications;
-
-import d7002d.Laboration4.NetAddr;
-import d7002d.Laboration4.Events.Segment;
-
-public interface Application {
-	public abstract String getName();
-	public abstract void handleSegment(NetAddr from, NetAddr to, Segment segment);
-}
